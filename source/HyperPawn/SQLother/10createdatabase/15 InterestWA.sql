45d10e68a8e0f421d0eaed179652b13d79607cd1
RayMetz100 <RayMetz100@hotmail.com>
2015-08-26 14:30:28 -0700

    change to interest, prep, storage, and firearm stroage fees

--- source/HyperPawn/SQLother/10createdatabase/15 InterestWA.sql
+++ source/HyperPawn/SQLother/10createdatabase/15 InterestWA.sql
@@ -24,7 +24,7 @@ INSERT INTO PawnFees_WA_Interest VALUES  (   .0001,              9.9999,1   ,NUL
                                          ( 70     ,             79.9999,3   ,NULL),
                                          ( 80     ,             89.9999,3.25,NULL),
                                          ( 90     ,             99.9999,3.5 ,NULL),
-                                         (100     ,922337203685477.5807,NULL,.03 )
+                                         (100     ,922337203685477.5807,NULL,.035 )
 
 
 IF OBJECT_ID('PawnFees_WA_Preparation') IS NOT NULL
@@ -37,7 +37,7 @@ CREATE TABLE PawnFees_WA_Preparation (
   AmountEnd         MONEY NOT NULL,
   PreparationAmount MONEY NOT NULL)
 
-INSERT INTO PawnFees_WA_Preparation VALUES(    .0001,              4.99  , 1.5 ),
+INSERT INTO PawnFees_WA_Preparation VALUES  (    .0001,              4.99  , 1.5 ),
                                             (   5     ,              9.99  , 3   ),
                                             (  10     ,             14.99  , 4   ),
                                             (  15     ,             19.99  , 4.5 ),
@@ -47,8 +47,8 @@ INSERT INTO PawnFees_WA_Preparation VALUES(    .0001,              4.99  , 1.5 )
                                             (  35     ,             39.99  , 6.5 ),
                                             (  40     ,             44.99  , 7   ),
                                             (  45     ,             49.99  , 7.5 ),
-                                            (  50     ,             54.99  , 8   ),
-                                            (  55     ,             59.99  , 8.5 ),
+                                            (  50     ,             54.99  , 7.5 ),
+                                            (  55     ,             59.99  , 8.25),
                                             (  60     ,             64.99  , 9   ),
                                             (  65     ,             69.99  , 9.5 ),
                                             (  70     ,             74.99  ,10   ),
@@ -65,34 +65,34 @@ INSERT INTO PawnFees_WA_Preparation VALUES(    .0001,              4.99  , 1.5 )
                                             ( 125     ,            129.99  ,14.75),
                                             ( 130     ,            149.99  ,15.50),
                                             ( 150     ,            174.99  ,15.75),
-                                            ( 175     ,            199.99  ,16   ),
-                                            ( 200     ,            224.99  ,17   ),
-                                            ( 225     ,            249.99  ,18   ),
-                                            ( 250     ,            274.99  ,19   ),
-                                            ( 275     ,            299.99  ,20   ),
-                                            ( 300     ,            324.99  ,21   ),
-                                            ( 325     ,            349.99  ,22   ),
-                                            ( 350     ,            374.99  ,23   ),
-                                            ( 375     ,            399.99  ,24   ),
-                                            ( 400     ,            424.99  ,25   ),
-                                            ( 425     ,            449.99  ,26   ),
-                                            ( 450     ,            474.99  ,27   ),
-                                            ( 475     ,            499.99  ,28   ),
+                                            ( 175     ,            199.99  ,17   ),
+                                            ( 200     ,            224.99  ,20   ),
+                                            ( 225     ,            249.99  ,22   ),
+                                            ( 250     ,            274.99  ,24   ),
+                                            ( 275     ,            299.99  ,25   ),
+                                            ( 300     ,            324.99  ,26   ),
+                                            ( 325     ,            349.99  ,27   ),
+                                            ( 350     ,            374.99  ,28   ),
+                                            ( 375     ,            399.99  ,29   ),
+                                            ( 400     ,            424.99  ,30   ),
+                                            ( 425     ,            449.99  ,31   ),
+                                            ( 450     ,            474.99  ,32   ),
+                                            ( 475     ,            499.99  ,33   ),
                                             ( 500     ,            524.99  ,29   ),
-                                            ( 525     ,            549.99  ,30   ),
-                                            ( 550     ,            599.99  ,31   ),
-                                            ( 600     ,            699.99  ,36   ),
-                                            ( 700     ,            799.99  ,41   ),
-                                            ( 800     ,            899.99  ,46   ),
-                                            ( 900     ,            999.99  ,51   ),
-                                            (1000     ,           1499.99  ,56   ),
-                                            (1500     ,           1999.99  ,61   ),
-                                            (2000     ,           2499.99  ,66   ),
-                                            (2500     ,           2999.99  ,71   ),
-                                            (3000     ,           3499.99  ,76   ),
-                                            (3500     ,           3999.99  ,81   ),
-                                            (4000     ,           4499.99  ,86   ),
-                                            (4500     ,922337203685477.5807,91   )
+                                            ( 525     ,            549.99  ,39   ),
+                                            ( 550     ,            599.99  ,40   ),
+                                            ( 600     ,            699.99  ,41   ),
+                                            ( 700     ,            799.99  ,46   ),
+                                            ( 800     ,            899.99  ,51   ),
+                                            ( 900     ,            999.99  ,56   ),
+                                            (1000     ,           1499.99  ,75   ),
+                                            (1500     ,           1999.99  ,81   ),
+                                            (2000     ,           2499.99  ,86   ),
+                                            (2500     ,           2999.99  ,91   ),
+                                            (3000     ,           3499.99  ,96   ),
+                                            (3500     ,           3999.99  ,101  ),
+                                            (4000     ,           4499.99  ,106  ),
+                                            (4500     ,922337203685477.5807,111  )
 
 GO
 
@@ -109,12 +109,20 @@ CREATE FUNCTION dbo.fn_PawnFees_WA
 RETURNS TABLE
 AS
 RETURN
-  SELECT ISNULL(MAX(MonthlyInterestAmount),CAST(0 AS MONEY)) MonthlyInterestAmount, ISNULL(MAX(PreparationAmount),CAST(0 AS MONEY)) PreparationAmount, CAST(3 AS MONEY) StorageFee, CAST(3 AS MONEY) FirearmFee
-  FROM (SELECT CAST(ISNULL(MonthlyInterestAmount,@PawnAmount * .03) AS MONEY) MonthlyInterestAmount, CAST(NULL AS MONEY) PreparationAmount
+  SELECT
+   ISNULL(MAX(MonthlyInterestAmount),CAST(0 AS MONEY)) MonthlyInterestAmount
+  ,ISNULL(MAX(PreparationAmount)    ,CAST(0 AS MONEY)) PreparationAmount
+  ,CAST(5 AS MONEY)                                    StorageFee
+  ,CAST(5 AS MONEY)                                    FirearmFee
+  FROM (SELECT
+         CAST(ISNULL(MonthlyInterestAmount,@PawnAmount * .035) AS MONEY) MonthlyInterestAmount
+        ,CAST(NULL AS MONEY)                                             PreparationAmount
         FROM   HyperPawnData.dbo.PawnFees_WA_Interest
         WHERE  @PawnAmount BETWEEN AmountStart AND AmountEnd
         UNION ALL 
-        SELECT CAST(NULL AS MONEY) MonthlyInterestAmount, PreparationAmount
+        SELECT
+		 CAST(NULL AS MONEY)                                             MonthlyInterestAmount
+		,PreparationAmount                                               PreparationAmount
         FROM   HyperPawnData.dbo.PawnFees_WA_Preparation 
         WHERE  @PawnAmount BETWEEN AmountStart AND AmountEnd) I
 GO
@@ -124,7 +132,6 @@ IF OBJECT_ID('TaxyGetPawnFees_WA_Interest') IS NOT NULL
   DROP PROC TaxyGetPawnFees_WA_Interest
 GO
 CREATE PROCEDURE TaxyGetPawnFees_WA_Interest
-WITH ENCRYPTION
 AS
   SELECT AmountStart, AmountEnd, ISNULL(MonthlyInterestAmount,0) MonthlyInterestAmount, ISNULL(MonthlyInterestPercent,0) MonthlyInterestPercent
   FROM   HyperPawnData.dbo.PawnFees_WA_Interest
@@ -134,7 +141,6 @@ IF OBJECT_ID('TaxyGetPawnFees_WA_Preparation') IS NOT NULL
   DROP PROC TaxyGetPawnFees_WA_Preparation
 GO
 CREATE PROCEDURE TaxyGetPawnFees_WA_Preparation
-WITH ENCRYPTION
 AS
   SELECT AmountStart, AmountEnd, PreparationAmount
   FROM   HyperPawnData.dbo.PawnFees_WA_Preparation
